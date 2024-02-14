6ea4f656f3aa343ef39491e44a1cc363a6c03f71
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-14 21:31:01 +0100

    country stats compo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CountryStatsComponent } from './pages/country-stats/country-stats.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, CountryStatsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/country-stats/country-stats.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, take } from 'rxjs';
+import { ILineChartsDatas } from 'src/app/core/models/ILineChartDatas';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+
+@Component({
+  selector: 'app-country-stats',
+  templateUrl: './country-stats.component.html',
+  styleUrls: ['./country-stats.component.scss']
+})
+export class CountryStatsComponent implements OnInit {
+
+  public olympics$: Observable<undefined | Olympic[]> = of(undefined);
+  public lineChartDatas$ : Observable<null | ILineChartsDatas> = of(null);
+
+  constructor(private olympicService: OlympicService) { }
+
+  ngOnInit(): void {
+    this.olympics$ = this.olympicService.getOlympics$()
+    this.lineChartDatas$ = this.olympicService.getCountryLineChartDatas$("france").pipe(take(1))
+  }
+
+}
