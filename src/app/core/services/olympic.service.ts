933ec5718bd30719c7f84bbecbcc2baea7fb5f12
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-14 20:24:22 +0100

    olympic services

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
+import { Participation } from '../models/Participation';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +15,7 @@ export class OlympicService {
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
@@ -25,7 +27,61 @@ export class OlympicService {
     );
   }
 
-  getOlympics() {
+  getOlympics$() {
     return this.olympics$.asObservable();
+  }
+
+  // using find - rxjs operator - would allow me to ignore emissions not matching my condition, 
+  // reduce - rxjs operator - would allow me to work on successive emissions
+  // it wouldn't allow me to find the first ICountryJOStats matching it
+  getCountryMedals$(country : string) : Observable<number>{
+    return this.getOlympics$().pipe( // !!! catch error
+        map((datas : Olympic[]) => datas
+        .find((datas : Olympic) => datas.country.toLowerCase() === country)?.participations
+        .reduce((accumulator : number, participation : Participation) => accumulator + participation.medalsCount, 0) || 0
+        )
+    )
+  }
+
+  getCountryTotalAthletes$(country : string) : Observable<number>{
+    return this.getOlympics$().pipe(
+        map((datas : Olympic[]) => datas
+        .find((datas : Olympic) => datas.country.toLowerCase() === country)?.participations
+        .reduce((accumulator : number, participation : Participation) => accumulator + participation.athleteCount, 0) || 0
+        )
+    )
+  }
+
+  getCountryLineChartDatas$(country : string) : Observable<ILineChartsDatas>{
+    return this.getOlympics$().pipe(
+        map((datas : Olympic[]) => {
+          const selectedCountryDatas = datas.find((datas) => datas.country.toLowerCase() === country)
+          if(selectedCountryDatas) return {name: country, series: selectedCountryDatas?.participations.map(participation => ({name : participation.year.toString(), value : participation.medalsCount}))}
+          return {name : country, series : [{name : '', value : 0 }]}
+        })
+    )
+  }
+
+  getPieChartDatas$() : Observable<{name : string, value : number} []>{
+    return this.getOlympics$().pipe(
+      map((datas : Olympic[]) => datas
+        .map((countryDatas : Olympic) => ({name : countryDatas.country, value : countryDatas.participations.reduce((accumulator : number, participation : ISingleEventStats) => accumulator + participation.medalsCount, 0)}))
+      )
+    )
+  }
+
+  getNumberOfJOs$() : Observable<number>{
+    return this.getOlympics$().pipe(
+      map((datas : Olympic[]) => {
+          let eventsDates : number[] = []
+          datas.forEach(countryStats => {
+            countryStats.participations.forEach(participation => {
+              if(!eventsDates.includes(participation.year)) eventsDates.push(participation.year)
+            })
+          })
+          return eventsDates.length
+        } 
+      )
+    )
   }
 }
