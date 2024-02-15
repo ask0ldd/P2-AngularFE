76514864fa1eb716a91374cb5289fcd4ad02e9a9
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-15 22:38:15 +0100

    detail : get chart datas from service

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -12,21 +12,38 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class DetailComponent implements OnInit {
 
-  countryName! : string | null
-
   public olympics$: Observable<undefined | Olympic[]> = of(undefined);
   public lineChartDatas$ : Observable<null | ILineChartsDatas> = of(null);
+
+  countryName : string = ""
+  totalMedals! : number // ! tell that the property will be assigned in OnInit
+  minYaxis! : number
+  maxYaxis! : number
+
+  chartDatas$!: Observable<ILineChartsDatas>
 
   constructor(private router:Router, private route: ActivatedRoute, private olympicService: OlympicService) { }
 
   ngOnInit(): void {
-    this.countryName = this.route.snapshot.paramMap.get('id')
-    if(this.countryName == null) {
+    /*this.olympics$ = this.olympicService.getOlympics$()
+    this.lineChartDatas$ = this.olympicService.getCountryLineChartDatas$("france").pipe(take(1))*/
+
+    this.countryName = "france"
+    if(this.countryName == null || this.countryName == "") {
       this.router.navigateByUrl('/404') 
       return
     }
-    this.olympics$ = this.olympicService.getOlympics$()
-    this.lineChartDatas$ = this.olympicService.getCountryLineChartDatas$("france").pipe(take(1))
+
+    this.chartDatas$ = this.olympicService.getCountryLineChartDatas$(this.countryName)
+
+    // It's not necessary for the variables depending on observables to be observables themselves : https://angular.io/guide/comparing-observables
+    this.chartDatas$.subscribe(datas => {
+      const medalsList = datas.series?.map(serie => serie.value)
+      this.minYaxis = Math.floor((Math.min(...medalsList) / 10)) * 10
+      if(this.minYaxis < 0) this.minYaxis = 0
+      this.maxYaxis = Math.ceil((Math.max(...medalsList) / 10)) * 10
+      this.totalMedals = datas.series.reduce((acc, serie) => acc + serie.value, 0)
+    })
   }
 
 }
